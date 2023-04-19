7e1a2f30f52c5ae65abe62249125ebde51625557
LucioDevv <114535199+LucioDevv@users.noreply.github.com>
2023-04-18 23:39:27 -0600

    Actualizado
    Todavia roto pero le puse los cambios que el profe sugirio.

--- Parcial 2/RockPaperScissors/RockPaperScissors.cpp
+++ Parcial 2/RockPaperScissors/RockPaperScissors.cpp
@@ -1,47 +1,76 @@
 #include <iostream>
 
 using namespace std;
-
+int PJug = 0;
+int PCom = 0;
 
 int main()
 {
-    srand(time(0));
-    int RPS = rand() % 3 + 1;
+    srand(time(NULL));
+    int RPS = (rand() % 3) + 1;
     int RPSHumano;
 
+    do {
+        RPS = (rand() % 3) + 1;
 
-    cout << "Piedra, papel, o tijera?" << endl << "1) Piedra" << endl << "2) Papel" << endl << "3) Tijera" << endl;
-    cout << "Valor de la computadora: " << RPS << endl;
+        cout << endl << "Piedra, papel, o tijera?" << endl << "1) Piedra" << endl << "2) Papel" << endl << "3) Tijera" << endl;
+        cout << "Puntuacion: Jugador " << PJug << " - " << PCom << " Computadora" << endl;
+        cout << "Valor de la computadora: " << RPS << endl;
 
-    cin >> RPSHumano;
+        cin >> RPSHumano;
 
-    if (RPSHumano == 1 || RPSHumano == 2 || RPSHumano == 3) {
-        if (RPS == 1 && RPSHumano == 2) {
-            cout << "Ganaste!";
+        if (RPSHumano == 1 || RPSHumano == 2 || RPSHumano == 3) {
+            if (RPS == 1 && RPSHumano == 2) {
+                system("cls");
+                cout << "Ganaste!";
+                PJug++;
+            }
+            else if (RPS == 1 && RPSHumano == 3) {
+                system("cls");
+                cout << "Perdiste..";
+                PCom++;
+            }
+            else if (RPS == 2 && RPSHumano == 1) {
+                system("cls");
+                cout << "Perdiste..";
+                PCom++;
+            }
+            else if (RPS == 2 && RPSHumano == 3) {
+                system("cls");
+                cout << "Ganaste!";
+                PJug++;
+            }
+            else if (RPS == 3 && RPSHumano == 1) {
+                system("cls");
+                cout << "Ganaste!";
+                PJug++;
+            }
+            else if (RPS == 3 && RPSHumano == 2) {
+                system("cls");
+                cout << "Perdiste..";
+                PCom++;
+            }
+            else {
+                system("cls");
+                cout << "Empataste.";
+            }
+        }
+        else
+        {
+            system("cls");
+            cout << "Inserte un valor valido la proxima vez.";
+        }
+    } while (PJug != 2 || PCom != 2);
 
-        }
-        else if (RPS == 1 && RPSHumano == 3) {
-            cout << "Perdiste..";
-        }
-        else if (RPS == 2 && RPSHumano == 1) {
-            cout << "Perdiste..";
-        }
-        else if (RPS == 2 && RPSHumano == 3) {
-            cout << "Ganaste!";
-        }
-        else if (RPS == 3 && RPSHumano == 1) {
-            cout << "Ganaste!";
-        }
-        else if (RPS == 3 && RPSHumano == 2) {
-            cout << "Perdiste..";
-        }
-        else {
-            cout << "Empataste.";
-        }
-    }
-    else
+    if (PJug == 2)
     {
-        cout << "Inserte un valor valido la proxima vez.";
+        cout << "Ganaste el juego!";
+        system("break");
     }
-    
+    else {
+        cout << "Perdiste el juego...";
+        system("break");
+    }
+
+
 }
