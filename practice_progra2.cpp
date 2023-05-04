8f58541e37d1fbc4727bd7dd4763038d86e123b1
LucioDevv <hardwiredpony@gmail.com>
2023-05-04 10:55:42 -0600

    iter

--- practice_progra2.cpp
+++ practice_progra2.cpp
@@ -1,17 +1,188 @@
 #include <iostream>
-#include<cstdlib> //Trabaja con números random
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cstdlib>
+
 using namespace std;
+
+int askNumber(string question, int high, int low = 1);
+//GUESS MY NUMBER
+void guessMyNumber();
+void vectorsPart1();
+void vectorReserve();
+
+
 
 int main()
 {
+    const int NUM_SCORES = 4;
+    int score;
 
+    vector<int>::const_iterator iter;
+
+    vector<int> scores;
+
+    for (int i = 0; i < NUM_SCORES; i++)
+    {
+        cout << "Score " << i + 1 << endl;
+        cin >> score;
+        scores.push_back(score);
+    }
+
+    cout << "Puntajes" << endl;
+    for (iter = scores.begin(); iter != scores.end(); iter++)
+    {
+        cout << *iter << endl;
+    }
+
+    /*Find*/
+    cout << "Buscar puntajes: Ingresa el puntaje que quieres buscar" << endl;
+    cin >> score;
+
+    iter = find(scores.begin(), scores.end(), score);
+
+    if (iter != scores.end())
+    {
+        cout << "\nTu puntaje se encuentra en el vector\n";
+    }
+    else
+    {
+        cout << "\nNo encontramos el puntaje que buscas\n";
+    }
+
+    /*Random Shuffle*/
+
+    srand(time(NULL));
+    random_shuffle(scores.begin(), scores.end());
+    cout << "\nScores Mezclados\n";
+    for (iter = scores.begin(); iter != scores.end(); iter++)
+    {
+        cout << *iter << endl;
+    }
+
+    //Shuffle String 
+    string word = "perro";
+    random_shuffle(word.begin(), word.end());
+    cout << word << endl;
+
+    /*Sort*/
+    cout << "\nCambiar orden \n";
+    sort(scores.begin(), scores.end());
+    for (iter = scores.begin(); iter != scores.end(); iter++)
+    {
+        cout << *iter << endl;
+    }
+
+    cout << "\nCambiar orden  greater\n";
+    sort(scores.begin(), scores.end(), greater<int>());
+    for (iter = scores.begin(); iter != scores.end(); iter++)
+    {
+        cout << *iter << endl;
+    }
+
+}
+
+
+
+
+void vectorReserve()
+{
+    vector<int> scores(10, 0);
+    cout << "Vector size is: " << scores.size() << endl;
+    cout << "Vector capacity is: " << scores.capacity() << endl;
+
+    scores.reserve(11);
+
+    cout << "Vector size is: " << scores.size() << endl;
+    cout << "Vector capacity is: " << scores.capacity() << endl;
+}
+
+
+void vectorsPart1()
+{
+    //vector<string> myStuff = { "espada", "martillo", "bomba" };
+    //vector<string> inventory(10);
+    //vector<string> inventory(10, " Vacio ");
+    //vector<string> inventory(myStuff);
+
+    vector<string> inventory;
+
+    inventory.push_back("Espada");
+    inventory.push_back("Escudo");
+    inventory.push_back("Martillo");
+
+    cout << "Tienes " << inventory.size() << " items en tu inventario" << endl;
+
+    //Iteration FOR
+    for (unsigned int i = 0; i < inventory.size(); i++)
+    {
+        cout << inventory[i] << endl;
+    }
+
+    cout << "\nCambiaste tu " << inventory[0] << " por una Bomba.";
+    inventory[0] = "Bomba";
+
+    cout << "\n";
+
+    //Iteration DO-WHILE
+    int i = 0;
+    do {
+        cout << inventory[i] << endl;
+        i++;
+    } while (i < inventory.size());
+
+    cout << inventory[0] << " tiene " << inventory[0].size() << " letras en él. ";
+
+    cout << " Perdiste un item en la última batalla" << endl;
+
+    //Delete item from vector
+    inventory.pop_back();
+    //Iteration FOR
+    for (unsigned int i = 0; i < inventory.size(); i++)
+    {
+        cout << inventory[i] << endl;
+    }
+
+    cout << "Un enemigo ha robado todas tus armas" << endl;
+    //inventory.clear();
+
+    if (inventory.empty())
+    {
+        cout << "No tienes nada" << endl;
+    }
+    else
+    {
+        for (unsigned int i = 0; i < inventory.size(); i++)
+        {
+            cout << inventory[i] << endl;
+        }
+    }
+}
+
+int askNumber(string question, int high, int low)
+{
+    int number = 0;
+
+    do {
+        cout << question << "entre " << low << " y " << high << endl;
+        cin >> number;
+    } while (number > high || number < low);
+
+    return number;
+}
+
+
+void guessMyNumber()
+{
     srand(static_cast<unsigned int>(time(0)));
     //srand(time(NULL));
     int randomNumber = rand();
-    int secretNumber = (randomNumber % 250) + 1;
+    int secretNumber = (randomNumber % 100) + 1;
     int guess;
     int tries = 0;
-    int veryClose
+    int veryClose;
+
 
     cout << "GUESS MY NUMBER" << endl;
     cout << "Adivina el número en el menor número de intentos posible." << endl;
@@ -20,15 +191,17 @@ int main()
     cout << secretNumber;
     do
     {
-        cout << "\nEnter a guess (1-250):" << endl;
-        cin >> guess;
+
+        guess = askNumber("\nIngresa un número ", 300);
+
         tries++;
 
-        veryClose = secretNumber - guess 
+        veryClose = secretNumber - guess;
 
-        if (veryClose <= 5 && >= 5 && veryClose != 0)
+
+        if (veryClose <= 5 && veryClose >= -5 && veryClose != 0)
         {
-            cout << "Muy Cerca\n"
+            cout << "Muy cerca!!\n";
         }
 
         if (guess > secretNumber) {
@@ -39,6 +212,7 @@ int main()
         }
         else {
             cout << "Excelente lo hiciste en -- " << tries << "-- intentos";
+
         }
     } while (guess != secretNumber);
 }
