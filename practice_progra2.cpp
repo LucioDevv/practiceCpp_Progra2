725710e988f9c33d3bb6fc5c823d0c014694c876
LucioDevv <114535199+LucioDevv@users.noreply.github.com>
2023-05-25 10:50:13 -0600

    Tic Tac Toe Avance

--- practice_progra2.cpp
+++ practice_progra2.cpp
@@ -10,6 +10,7 @@ int askNumber(string question, int high, int low = 1);
 //GUESS MY NUMBER
 void guessMyNumber();
 
+void MatrixBox();
 void vectorsPart1();
 void vectorReserve();
 void iterators();
@@ -23,6 +24,148 @@ void display(const vector<string>& vec);
 
 int main()
 {
+    const int ROWS = 3;
+    const int COLUMNS = 3;
+    int playerTurn = 0;
+    bool winAchieved = false;
+    int playerInput;
+
+    char board[ROWS][COLUMNS] = { {' ', ' ', ' '},
+                                  {' ', ' ', ' '},
+                                  {' ', ' ', ' '} };
+
+    char exampleBoard[ROWS][COLUMNS] = { {'0', '1', '2'},
+                                         {'3', '4', '5'},
+                                         {'6', '7', '8'} };
+
+    cout << endl << "You are X!" << endl;
+
+    do {
+
+        if (playerTurn == 0) {
+            cout << "---TIC TAC TOE---" << endl;
+            for (int i = 0; i < ROWS; i++) {
+
+                for (int j = 0; j < COLUMNS; j++) {
+                    cout << board[i][j] << "   ";
+                }
+                cout << endl;
+
+            }
+
+            cout << endl << "Where would you like to place your X? (0 - 8)" << endl;
+            for (int i = 0; i < ROWS; i++) {
+
+                for (int j = 0; j < COLUMNS; j++) {
+                    cout << exampleBoard[i][j] << "   ";
+                }
+                cout << endl;
+
+            }
+            cin >> playerInput;
+
+            if (playerInput >= 0 && playerInput <= 2) {
+                if (board[0][playerInput] == ' ') {
+                    board[0][playerInput] = 'X';
+                    playerTurn = 1;
+                }
+                else {
+                    cout << "Ya hay un simbolo alli, intente otro." << endl;
+                    playerTurn = 0;
+                }
+            }
+            else if (playerInput >= 3 && playerInput <= 5) {
+                playerInput = playerInput - 3;
+                if (board[1][playerInput] == ' ') {
+                    board[1][playerInput] = 'X';
+                    playerTurn = 1;
+                }
+                else {
+                    cout << "Ya hay un simbolo alli, intente otro." << endl;
+                    playerTurn = 0;
+                }
+            }
+            else if (playerInput >= 6 && playerInput <= 8) {
+                playerInput = playerInput - 6;
+                if (board[2][playerInput] == ' ') {
+                    board[2][playerInput] = 'X';
+                    playerTurn = 1;
+                }
+                else {
+                    cout << "Ya hay un simbolo alli, intente otro." << endl;
+                    playerTurn = 0;
+                }
+            }
+            else {
+                cout << "Inserte un valor valido." << endl;
+                playerTurn = 0;
+            }
+        }
+        else {
+            cout << "---TIC TAC TOE---" << endl;
+            for (int i = 0; i < ROWS; i++) {
+
+                for (int j = 0; j < COLUMNS; j++) {
+                    cout << board[i][j] << "   ";
+                }
+                cout << endl;
+
+            }
+
+            cout << endl << "Where would you like to place your O? (0 - 8)" << endl;
+            for (int i = 0; i < ROWS; i++) {
+
+                for (int j = 0; j < COLUMNS; j++) {
+                    cout << exampleBoard[i][j] << "   ";
+                }
+                cout << endl;
+
+            }
+            cin >> playerInput;
+
+            if (playerInput >= 0 && playerInput <= 2) {
+                if (board[0][playerInput] == ' ') {
+                    board[0][playerInput] = 'O';
+                    playerTurn = 0;
+                }
+                else {
+                    cout << "Ya hay un simbolo alli, intente otro." << endl;
+                    playerTurn = 1;
+                }
+            }
+            else if (playerInput >= 3 && playerInput <= 5) {
+                playerInput = playerInput - 3;
+                if (board[1][playerInput] == ' ') {
+                    board[1][playerInput] = 'O';
+                    playerTurn = 0;
+                }
+                else {
+                    cout << "Ya hay un simbolo alli, intente otro." << endl;
+                    playerTurn = 1;
+                }
+            }
+            else if (playerInput >= 6 && playerInput <= 8) {
+                playerInput = playerInput - 6;
+                if (board[2][playerInput] == ' ') {
+                    board[2][playerInput] = 'O';
+                    playerTurn = 0;
+                }
+                else {
+                    cout << "Ya hay un simbolo alli, intente otro." << endl;
+                    playerTurn = 1;
+                }
+            }
+            else {
+                cout << "Inserte un valor valido." << endl;
+                playerTurn = 1;
+            }
+        }
+
+    } while (winAchieved == false);
+
+}
+
+void MatrixBox() {
 
     const int ROWS = 11;
     const int COLUMNS = 11;
@@ -37,7 +180,8 @@ int main()
             if (j == 0 || j == COLUMNS - 1)
             {
                 cout << " 1 ";
-            } else if (i == 0 || i == ROWS - 1)
+            }
+            else if (i == 0 || i == ROWS - 1)
             {
                 cout << " 1 ";
             }
@@ -60,7 +204,6 @@ int main()
         }
         cout << endl;
     }
-
 }
 
 void badSwap(int x, int y)
